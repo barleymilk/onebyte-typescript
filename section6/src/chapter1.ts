69c17976e2f12e3da801e90f1717e654ed5eca1f
barleymilk <barleymilk640@gmail.com>
2025-03-26 14:17:51 +0900

    commit

---
+++ section6/src/chapter1.ts
@@ -0,0 +1,59 @@
+/*
+ * 타입스크립트의 클래스
+ */
+
+const employee = {
+  name: "이정환",
+  age: 27,
+  position: "developer",
+  work() {
+    console.log("일함");
+  },
+};
+
+class Employee {
+  // 필드
+  name: string;
+  age: number;
+  position: string;
+
+  // 생성자
+  constructor(name: string, age: number, position: string) {
+    this.name = name;
+    this.age = age;
+    this.position = position;
+  }
+
+  // 메서드
+  work() {
+    console.log("일함");
+  }
+}
+
+const employeeB = new Employee("이정환", 27, "개발자");
+console.log(employeeB);
+
+// 클래스를 타입으로도 활용할 수 있다.
+const employeeC: Employee = {
+  name: "",
+  age: 0,
+  position: "",
+  work() {},
+};
+
+// 확장해보자
+class ExecutiveOfficer extends Employee {
+  // 필드
+  officeNumber: number;
+
+  // 생성자
+  constructor(
+    name: string,
+    age: number,
+    position: string,
+    officeNumber: number
+  ) {
+    super(name, age, position);
+    this.officeNumber = officeNumber;
+  }
+}
