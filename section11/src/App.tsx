3caec33e13f82b7699ed676be8a5ac11709d41aa
barleymilk <barleymilk640@gmail.com>
2025-09-28 18:45:29 +0900

    section11 commit

---
+++ section11/src/App.tsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import { useState, useRef, useEffect } from "react";
+import { Todo } from "./types";
+import Editor from "./components/Editor";
+import TodoItem from "./components/TodoItem";
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  const onClickDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd} />
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo} onClickDelete={onClickDelete} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
